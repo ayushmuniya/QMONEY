fa662599218acd8d96b22c1adc961f4a65e048eb
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-12-08 23:26:47 +0530

    Create AnnualReturnsController.java

---
+++ annual-return-app/src/main/java/com/crio/stock/annualreturnapp/AnnualReturnsController.java
@@ -0,0 +1,45 @@
+package com.crio.stock.annualreturnapp;
+
+import com.crio.warmup.stock.exception.StockQuoteServiceException;
+// import com.crio.warmup.stock.exception.JsonProcessingException;
+import com.crio.warmup.stock.portfolio.PortfolioManager;
+import com.crio.warmup.stock.portfolio.PortfolioManagerFactory;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/portfolio")
+public class AnnualReturnsController {
+
+  private RestTemplate restTemplate;
+  private int numThreads = 10;
+
+  @Autowired
+  public AnnualReturnsController(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
+
+  @PostMapping("/analyze")
+  @ResponseBody
+  public PortfolioResponse calculateReturns(@RequestBody Portfolio portfolio) throws InterruptedException {
+    PortfolioManager portfolioManager = PortfolioManagerFactory.getPortfolioManager(null, restTemplate);
+    LocalDate endDate = LocalDate.now().minus(1, ChronoUnit.DAYS);
+    try {
+      return PortfolioResponse.builder().annualizedReturns(portfolioManager.calculateAnnualizedReturn(portfolio.getPortfolioTrades(), endDate)).calculationsDate(endDate).name(portfolio.getName()).build();
+    } catch (StockQuoteServiceException e) {
+      e.printStackTrace();
+      throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Provider error", e);
+    }
+  }
+}
